41593e27672af9db24665343db42fdc0cc7c5bac
Minyoung GWON <sinkuu@naver.com>
2023-10-25 15:12:36 +0200

    Créé le RESTful API

--- backend/server.js
+++ backend/server.js
@@ -1,48 +1,27 @@
-const http = require('http');
-const app = require('./app');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const normalizePort = val => {
-    const port = parseInt(val, 10);
+const bookRoutes = require('./routes/book');
+const configMongo = require('./dev')
 
-    if (isNaN(port)) {
-        return val;
-    }
-    if (port >= 0) {
-        return port;
-    }
-    return false;
-}
-const port = normalizePort(process.env.PORT || '3000');
+const app = express();
 
-app.set('port', port);
+mongoose.connect(configMongo.mongoURI,
+    { useNewUrlParser: true,
+    useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-const errorHandler = error => {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
-    const address = server.address();
-    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elavated priviledges.');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.log(bind + ' is already in use.');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
-};
+app.use(express.json());
 
-const server = http.createServer(app);
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+    });
 
-server.on('error', errorHandler);
-server.on('listening', () => {
-    const address = server.address();
-    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-    console.log('Listening on ' + bind);
-});
+app.use('/api/books', bookRoutes);
 
-server.listen(port);
+app.listen(3000)
