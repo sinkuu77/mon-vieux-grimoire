0119f14e555c05466fb115585d53c05fcc6b49dc
Minyoung GWON <sinkuu@naver.com>
2023-10-29 21:51:32 +0100

    Modifié le controleur de modification de livre

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -6,10 +6,11 @@ const multer = require('../middleware/multer-config');
 
 const bookCtrl = require('../controllers/book');
 
-router.post('/', auth, multer, bookCtrl.createBook);
-router.put('/:id', auth, bookCtrl.modifyBook);
-router.delete('/:id', auth, bookCtrl.deleteBook);
-router.get('/:id', bookCtrl.getOneBook);
 router.get('/', bookCtrl.getAllBooks);
+router.post('/', auth, multer, bookCtrl.createBook);
+router.get('/:id', bookCtrl.getOneBook);
+router.put('/:id', auth, multer, bookCtrl.modifyBook);
+router.delete('/:id', auth, bookCtrl.deleteBook);
+
 
 module.exports = router;
