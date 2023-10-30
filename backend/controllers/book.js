c0bc1dff3dc3121863944a4e49a592c833dceb75
Minyoung GWON <sinkuu@naver.com>
2023-10-30 18:42:27 +0100

    Ajouté une fonction de convertir des images

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -1,25 +1,49 @@
 const Book = require('../models/Book');
 const fs = require('fs');
+const sharp = require('sharp');
+
+const MIME_TYPES = {
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpg',
+    'image/png': 'png'
+}
+
+const convertImg = async(file) => {
+    const { buffer, originalname } = file;
+    const timestamp = Date.now();
+    const newOriginalname = originalname.split(MIME_TYPES[file.mimetype])[0]
+    const filename = `${timestamp}-${newOriginalname}webp`
+
+    await sharp(buffer)
+    .webp({ quality: 20 })
+    .toFile('./images/' + filename);
+
+    return filename;
+}
 
 
-exports.createBook = (req, res, next) => {
+exports.createBook = async(req, res, next) => {
     const bookObject = JSON.parse(req.body.book);
     delete bookObject._id;
     delete bookObject._userId;
+    
+    const filename = await convertImg(req.file);
+    
     const book = new Book({
         ...bookObject,
         userId: req.auth.userId,
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${filename}`
     });
     book.save()
     .then(() =>{ res.status(201).json({ message: 'Livre enregistré !'})})
     .catch(error => {res.status(400).json( { error } )});
 };
 
-exports.modifyBook = (req, res, next) => {
+exports.modifyBook = async(req, res, next) => {
+    const filename = await convertImg(req.file);
     const bookObject = req.file ? {
         ...JSON.parse(req.body.book),
-        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${filename}`
     } : { ...req.body };
     delete bookObject._userId;
     Book.findOne({_id: req.params.id})
--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,21 +1,7 @@
 const multer = require('multer');
 
-const MIME_TYPES = {
-    'image/jpg': 'jpg',
-    'image/jpeg': 'jpg',
-    'image/png': 'png',
-    'image/webp': 'webp'
-}
 
-const storage = multer.diskStorage({
-    destination: (req, file, callback) => {
-        callback(null, 'images')
-    },
-    filename: (req, file, callback) => {
-        const name = file.originalname.split(' ').join('_');
-        const extension = MIME_TYPES[file.mimetype];
-        callback(null, name + Date.now() + '.' + extension);
-    }
-})
+const storage = multer.memoryStorage();
+
 
 module.exports = multer({ storage: storage }).single('image');
